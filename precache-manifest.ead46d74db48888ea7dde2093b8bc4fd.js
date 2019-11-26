0d3be40a6c455ca9254d5b97a3b63f08b8a7a4ef
jmreguyal <josemarireguyal@gmail.com>
2019-11-26 19:53:45 +0800

    Updates

---
+++ precache-manifest.ead46d74db48888ea7dde2093b8bc4fd.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "85a57a5e0f4716057e3e2d5fe00cf45e",
+    "url": "/robofriends/index.html"
+  },
+  {
+    "revision": "26dafc4245b14a37f593",
+    "url": "/robofriends/static/css/2.eb45ad44.chunk.css"
+  },
+  {
+    "revision": "7f5f654328960de5ea67",
+    "url": "/robofriends/static/css/main.3df96678.chunk.css"
+  },
+  {
+    "revision": "26dafc4245b14a37f593",
+    "url": "/robofriends/static/js/2.1f39b60e.chunk.js"
+  },
+  {
+    "revision": "7f5f654328960de5ea67",
+    "url": "/robofriends/static/js/main.19e7177a.chunk.js"
+  },
+  {
+    "revision": "7b4f849e958ac4107787",
+    "url": "/robofriends/static/js/runtime-main.a1071882.js"
+  },
+  {
+    "revision": "7a814d9d8801b9532895fd2e468e1a06",
+    "url": "/robofriends/static/media/KaushanScript-Regular.7a814d9d.otf"
+  }
+]);
